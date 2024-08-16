fb36521d1a9e3dc5a0c654b7437b2895c5f5b19f
İsmail <ismailll.ozcelik@gmail.com>
2024-08-16 21:28:17 +0300

    white-label ticket buying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'white-label-passo';
+  title = 'White-label Counter App';
+  brandName = 'Default Brand';
+  count = 0;
+  themeStyle = {
+    backgroundColor: 'white',
+    color: 'black',
+    padding: '20px',
+    textAlign: 'center',
+  };
+
+  ngOnInit() {
+    const hostname = typeof window !== 'undefined' ? window.location.hostname : '';
+    
+    if (hostname.includes('gs')) {
+      this.setupForGs();
+    } else if (hostname.includes('bjk')) {
+      this.setupForBjk();
+    }
+  }
+
+  setupForGs() {
+    this.brandName = 'Galatasaray';
+    this.themeStyle = {
+      backgroundColor: 'red',
+      color: 'yellow',
+      padding: '20px',
+      textAlign: 'center',
+    };
+  }
+
+  setupForBjk() {
+    this.brandName = 'Beşiktaş';
+    this.themeStyle = {
+      backgroundColor: 'black',
+      color: 'white',
+      padding: '20px',
+      textAlign: 'center',
+    };
+  }
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
 }
